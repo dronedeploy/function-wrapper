baf37172daa93d5bb5bda94553a3655938a2332d
Clay Koberlein <clay@dronedeploy.com>
2018-05-17 17:40:00 -0500

    Minor adjustment for when env is explicitly prod

--- helpers/api.js
+++ helpers/api.js
@@ -1,5 +1,5 @@
 exports.getBaseUrl = () => {
-  // If DDENV is defined, use it, otherwise default to prod
-  const env = process.env.DDENV ? "_" + process.env.DDENV : "";
-  return `https://api${env}.dronedeploy.com`
+  const env = process.env.DDENV || "prod";  // default to prod
+  const envUrlPart = env === "prod" ? "" : "_" + env;
+  return `https://api${envUrlPart}.dronedeploy.com`
 }
