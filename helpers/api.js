0ee26d59f794951e9bd6363f806b0b421f1fdfe1
Clay Koberlein <clay@dronedeploy.com>
2018-05-17 17:29:37 -0500

    Fix base url usage

--- helpers/api.js
+++ helpers/api.js
@@ -1,4 +1,5 @@
 exports.getBaseUrl = () => {
-  const env = process.env.DDENV === "prod" ? "" : "_" + process.env.DDENV;
+  // If DDENV is defined, use it, otherwise default to prod
+  const env = process.env.DDENV ? "_" + process.env.DDENV : "";
   return `https://api${env}.dronedeploy.com`
 }
